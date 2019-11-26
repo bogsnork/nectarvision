8fcaafd008e06a45368603d0593a85540c6a5faa
CK <christoph.kratz@naturalengland.org.uk>
2019-11-26 15:32:47 +0000

    amend data_prep to import json output from VoTT

--- R/data_prep.R
+++ R/data_prep.R
@@ -4,116 +4,76 @@
 
 #load packages ----
 library(tidyverse)
-library(keras)
 library(rjson)
-# library(magick) #may need terminal command: sudo apt-get install libmagick++-dev
 library(repurrrsive)
 library(listviewer)
-# library(magrittr)
 
 
 #data sources ----
 
 #define image directories 
-img_dir <- "~/compvis_flowers_1/data/quadrats/images_voc"
-annot_coco_file <- "../data/quadrats/export-coco-2019-03-05T14_31_52.619981.json"
+img_dir <- "..."
+annot_coco_file <- "annotations/nectarvision_proj-export.json"
+annot_csv_file <- "annotations/nectarvision_proj-export.csv"
 
-#image attributes----
-
-#image annotations
+#image annotations - from json
 annotations <- fromJSON(file = annot_coco_file)
-#str(annotations, max.level = 1)
 
-imageinfo <- annotations$images %>% {
+imageinfo <- annotations$assets %>% map("asset") %>% {
   tibble(
-    id = map_chr(., "id"),
-    file_name = map_chr(., "file_name"),
-    image_height = map_dbl(., "height"),
-    image_width = map_dbl(., "width")
-  )
-}
+  image_id = map_chr(., "id"),
+  file_name = map_chr(., "name"),
+  image_height = map_dbl(map(., "size"), "height"),
+  image_width = map_dbl(map(., "size"), "width"),
+  )}
 
-#bounding box coordinates
-boxinfo <- annotations$annotations %>% {
-  tibble(
-    image_id = map_chr(., "image_id"),
-    category_id = map_dbl(., "category_id"),
-    bbox = map(., "bbox")
-  )
-}
 
-boxinfo <- boxinfo %>% 
-  mutate(bbox = unlist(map(.$bbox, function(x) paste(x, collapse = " "))))
-boxinfo <- boxinfo %>% 
-  separate(bbox, into = c("x_left", "y_top", "bbox_width", "bbox_height"))
-boxinfo <- boxinfo %>% mutate_at(vars(-image_id), as.numeric) 
-#For the bounding boxes, the annotation file provides x_left and y_top coordinates, as well as width and height. We will mostly be working with corner coordinates, so we create the missing x_right and y_top.
+#regions, rectangles and tags
 
-#As usual in image processing, the y axis starts from the top.
-boxinfo <- boxinfo %>% 
-  mutate(y_bottom = y_top + bbox_height - 1, x_right = x_left + bbox_width - 1)
+regions <- annotations$assets %>% map("regions") %>% 
+  tibble(image_id = names(annotations$assets), regions = .) %>% 
+  unnest_longer(data = ., col = regions) %>% 
+  unnest_wider(data = ., col = regions)
+               
+regions <- select(regions, image_id, region_id = "id", tags, boundingBox, points)               
+regions_bkp <- regions
+regions <- regions %>% unnest_wider(data = ., col = boundingBox) 
+regions <- select(regions, -`...1`) 
+regions_bkp <- regions
+regions <- unnest_longer(regions, tags)
+regions_bkp <- regions
 
-# match class ids to class names.
-catinfo <- annotations$categories %>%  {
-  tibble(id = map_dbl(., "id"), name = map_chr(., "name"))
-}
-catinfo
+#bounding boxes
+boxinfo <- regions %>% 
+  mutate(x_right = width + left,
+         y_bottom = top - height) %>% 
+  select(region_id, image_id,
+         category = tags,
+         x_left = left,
+         x_right,
+         y_top = top,
+         y_bottom,
+         bbox_width = width,
+         bbox_height = height)
 
-#merge image info with bounding box and class info
-imageinfo <- imageinfo %>%
-  inner_join(boxinfo, by = c("id" = "image_id")) %>%
-  inner_join(catinfo, by = c("category_id" = "id"))
 
-#get image name
-annot_csv <- read.csv(annot_csv_file) %>% 
-  select(ID, External.ID)
 
-imageinfo <- imageinfo %>%
-  inner_join(annot_csv, by = c("id" = "ID")) %>% 
-  rename(file_name_original = External.ID,
-         url = file_name) %>% 
-  mutate(file_name = paste0(id, str_sub(file_name_original, start = -4)))
-
-#clean data----
-#remove all rows where we don't have an image
-img_files <- list.files(img_dir)
-img_files <- img_files[which(str_sub(img_files, start = -4) != ".xml")]
-length(img_files)
-
-imageinfo <- imageinfo %>% 
-  filter(file_name %in% img_files) %>% 
-  droplevels()
+# #clean data----
+# #remove all rows where we don't have an image
+# img_files <- list.files(img_dir)
+# img_files <- img_files[which(str_sub(img_files, start = -4) != ".xml")]
+# length(img_files)
+# 
+# imageinfo <- imageinfo %>% 
+#   filter(file_name %in% img_files) %>% 
+#   droplevels()
 
 #tidy up and export----
+write_rds(boxinfo, "data/boxinfo.rds")
+write_rds(imageinfo, "data/imageinfo.rds")
+write_rds(regions, "data/regions.rds")
 
-
-#write.csv(imageinfo, "../data/quadrats/imageinfo.csv")
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+write.csv(imageinfo, "data/imageinfo.csv", row.names = F)
+write.csv(boxinfo, "data/boxinfo.csv", row.names = F)
 
 
