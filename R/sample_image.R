b506b175887aa8fd7d919e5d4508e99521fa151a
CK <christoph.kratz@gmail.com>
2020-02-17 09:37:06 +0000

    nice labels #11

--- R/sample_image.R
+++ R/sample_image.R
@@ -1,6 +1,5 @@
 #function to print sample image
 
-
 example <- imageinfo4ssd[50,]
 
 category <- (example$categories %>% str_split(pattern = ", "))[[1]]
@@ -10,17 +9,32 @@ x_right <- (example$xr_orig %>% str_split(pattern = ", "))[[1]]
 y_top <- (example$yt_orig %>% str_split(pattern = ", "))[[1]]
 y_bottom <- (example$yb_orig %>% str_split(pattern = ", "))[[1]]
 
+cex <- 4
+## Background rectangle: 
+
+pad <- 0.1 #proportion to pad box by
+ 
 img <- image_read(file.path(img_dir, example$file_name))
 img <- image_draw(img)
+textHeight <- graphics::strheight(category, cex = cex)
+textWidth <- graphics::strwidth(category, cex = cex)
 for (i in 1:example$cnt) {
   rect(xleft = x_left[i], ybottom = y_bottom[i],
        xright = x_right[i], ytop = y_top[i],
        border = "white", lwd = 5)
-  text(
-    x = as.integer(x_right[i]), y = as.integer(y_top[i]), labels = category[i], 
-    cex = 4, adj = c(1,0), col = "white")
+ rect(xleft = as.integer(x_right[i]) - (textWidth[i] * (1 + pad)), 
+     ybottom = as.integer(y_top[i]) - (textHeight[i] * (1 + pad)), 
+     xright = as.integer(x_right[i]) + (textWidth[i] * pad), 
+     ytop = as.integer(y_top[i]) + (textHeight[i] * pad),
+     col = "white", border = NA) 
+ text(x = as.integer(x_right[i]), y = as.integer(y_top[i]), labels = category[i], 
+    cex = cex, 
+    adj = c(1,1), 
+    col = "black", vfont = c("sans serif", "bold"))
 }
+
 dev.off()
 print(img)
+
 
 
